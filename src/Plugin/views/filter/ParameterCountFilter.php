6cfd56b659bf33a7bbfbe81ab3827b975bf5f883
stopopol <christoph.wohner@umweltbundesamt.at>
2026-06-09 08:24:45 +0200

    Add files via upload

---
+++ src/Plugin/views/filter/ParameterCountFilter.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Drupal\deims_routines\Plugin\views\filter;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\views\Plugin\views\filter\FilterPluginBase;
+
+/**
+ * Filter nodes by the number of field_parameters taxonomy term references.
+ *
+ * @ViewsFilter("parameter_count_filter")
+ */
+class ParameterCountFilter extends FilterPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function adminLabel($short = FALSE) {
+    return $this->t('Parameter count filter');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+    $options['operator'] = ['default' => 'gt'];
+    $options['value'] = ['default' => 5];
+    return $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function operatorOptions() {
+    return [
+      'gt' => $this->t('Is greater than'),
+      'gte' => $this->t('Is greater than or equal to'),
+      'lt' => $this->t('Is less than'),
+      'lte' => $this->t('Is less than or equal to'),
+      'eq' => $this->t('Is equal to'),
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::buildOptionsForm($form, $form_state);
+
+    $form['operator'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Operator'),
+      '#options' => $this->operatorOptions(),
+      '#default_value' => $this->options['operator'],
+    ];
+
+    $form['value'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Number of parameter values'),
+      '#default_value' => $this->options['value'],
+      '#min' => 0,
+      '#step' => 1,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildExposeForm(&$form, FormStateInterface $form_state) {
+    parent::buildExposeForm($form, $form_state);
+
+    $form['operator'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Operator'),
+      '#options' => $this->operatorOptions(),
+      '#default_value' => $this->options['operator'],
+    ];
+
+    $form['value'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Number of parameter values'),
+      '#default_value' => $this->options['value'],
+      '#min' => 0,
+      '#step' => 1,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $this->ensureMyTable();
+
+    $field_table = 'node__field_parameters';
+    $count = (int) $this->options['value'];
+
+    $having_operator = $this->havingOperator($this->options['operator']);
+
+    // Build a subquery selecting entity_ids whose field_parameters count
+    // satisfies the configured operator and threshold.
+    $subquery = \Drupal::database()->select($field_table, 'fp')
+      ->fields('fp', ['entity_id'])
+      ->condition('fp.deleted', 0)
+      ->groupBy('fp.entity_id')
+      ->having("COUNT(fp.entity_id) $having_operator $count");
+
+    // Restrict the view's base table (node_field_data) to those entity_ids.
+    $this->query->addWhere(
+      $this->options['group'],
+      'node_field_data.nid',
+      $subquery,
+      'IN'
+    );
+  }
+
+  /**
+   * Maps option operator keys to SQL comparison operators.
+   */
+  protected function havingOperator(string $key): string {
+    $map = [
+      'gt'  => '>',
+      'gte' => '>=',
+      'lt'  => '<',
+      'lte' => '<=',
+      'eq'  => '=',
+    ];
+    return $map[$key] ?? '>';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function canExpose() {
+    return TRUE;
+  }
+
+}
