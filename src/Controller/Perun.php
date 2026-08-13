e6b02c72327611ab9108b5b9cb776a567cf261f0
stopopol <christoph.wohner@umweltbundesamt.at>
2026-08-13 11:52:05 +0200

    Update Perun.php

--- src/Controller/Perun.php
+++ src/Controller/Perun.php
@@ -130,7 +130,7 @@ class Perun extends ControllerBase {
 
       // 3. Push the full modified array back to Perun
       $result = $this->updateFormItems($form_items);
-      if ($result < 0) {
+      if ($result > 0) {
         \Drupal::logger('deims_routines')->info('DEIMS Site List successfully updated on PERUN.');
       }
       else {
